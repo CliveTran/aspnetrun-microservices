35aecd0ae1ac00721c3ef61baf82183def379376
Clive <tranvinhnhan.tech.clone@gmail.com>
2021-09-08 17:39:12 +0700

    create discount grpc service

---
+++ src/services/discount/discount.grpc/Infrastructure/Repositories/IDiscountRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using discount.grpc.Domain.Entities;
+
+namespace discount.grpc.Infrastructure.Repositories;
+public interface IDiscountRepository
+{
+    Task<Coupon> GetDiscount(string productName);
+
+    Task<bool> CreateDiscount(Coupon coupon);
+
+    Task<bool> UpdateDiscount(Coupon coupon);
+
+    Task<bool> DeleteDiscount(string productName);
+}
---
+++ src/services/discount/discount.grpc/Services/DiscountService.cs
@@ -0,0 +1,90 @@
+﻿
+using discount.grpc.Domain.Entities;
+using discount.grpc.Infrastructure.Repositories;
+using discount.grpc.Protos;
+using Grpc.Core;
+
+namespace discount.grpc.Services;
+public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+{
+    private readonly IDiscountRepository _repository;
+    private readonly ILogger<DiscountService> _logger;
+
+    public DiscountService(IDiscountRepository repository, ILogger<DiscountService> logger)
+    {
+        _repository = repository;
+        _logger = logger;
+    }
+
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await _repository.GetDiscount(request.ProductName);
+        if (coupon == null)
+        {
+            throw new RpcException(new Status(StatusCode.NotFound, $"Discount with ProductName={request.ProductName} is not found."));
+        }
+
+        _logger.LogInformation($"Discount is retrieved for ProductName: {coupon.ProductName}, Amount: {coupon.Amount}");
+
+        return new CouponModel
+        {
+            Id = coupon.Id,
+            ProductName = coupon.ProductName,
+            Description = coupon.Description,
+            Amount = coupon.Amount
+        };
+    }
+
+    public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = new Coupon
+        {
+            Id = request.Coupon.Id,
+            ProductName = request.Coupon.ProductName,
+            Description = request.Coupon.Description,
+            Amount = request.Coupon.Amount
+        };
+
+        var isCreated = await _repository.CreateDiscount(coupon);
+
+        if (!isCreated)
+        {
+            throw new RpcException(new Status(StatusCode.Internal, $"Discount cannot be created with ProductName={request.Coupon.ProductName}."));
+        }
+
+        _logger.LogInformation($"Discount is created succesfully. ProductName: {request.Coupon.ProductName}");
+        return new CouponModel
+        {
+            Id = coupon.Id,
+            ProductName = coupon.ProductName,
+            Description = coupon.Description,
+            Amount = coupon.Amount
+        };
+    }
+
+    public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = new Coupon
+        {
+            Id = request.Coupon.Id,
+            ProductName = request.Coupon.ProductName,
+            Description = request.Coupon.Description,
+            Amount = request.Coupon.Amount
+        };
+        await _repository.UpdateDiscount(coupon);
+
+        return new CouponModel
+        {
+            Id = coupon.Id,
+            ProductName = coupon.ProductName,
+            Description = coupon.Description,
+            Amount = coupon.Amount
+        };
+    }
+
+    public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        return new DeleteDiscountResponse { Success = await _repository.DeleteDiscount(request.ProductName) };
+    }
+}
+
