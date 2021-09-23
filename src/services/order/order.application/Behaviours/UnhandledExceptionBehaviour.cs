8e408a2f8a35cf605e6f96a259833e72caba5d33
Clive <tranvinhnhan.tech.clone@gmail.com>
2021-09-23 15:03:52 +0700

    di application layer

---
+++ src/services/order/order.application/Behaviours/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace order.application.Behaviours
+{
+    public class UnhandledExceptionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public UnhandledExceptionBehaviour(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Exception: Unhandled exception for Request {typeof(TRequest).Name}.", typeof(TRequest).Name, request);
+                throw;
+            }
+        }
+    }
+}
---
+++ src/services/order/order.application/ServicesRegistration.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using order.application.Behaviours;
+using System.Reflection;
+
+namespace order.application
+{
+    public static class ServicesRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly()); 
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return services;
+        }
+    }
+}
